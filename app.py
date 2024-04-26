b5ba804ddc2255910345be85602d4cea49c5575f
Rodriasd <veronrodrigo98@gmail.com>
2024-04-26 10:03:41 -0300

    was added graphic of cake

--- app.py
+++ app.py
@@ -1,19 +1,16 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+from random import sample 
 
 
 df = pd.DataFrame({ "x": [1,2,3,4,5], "y": [6,7,8,9,0]})
 plt.plot(df["x"], df["y"])
 plt.title("Título con pandas")
+plt.grid(True)
+
+#Grafico de barras
+data = sample(range(1, 1000), 100)
+plt.hist(data,bins = 10)
+plt.title("Histograma")
 plt.show()
-
-
-# x = np.linspace(0,5, 11)
-# y = x ** 2
-
-
-# plt.plot(x, y)
-# plt.title('a')
-# plt.grid(True)
-# plt.show() 
