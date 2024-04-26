0784479036c556a4c355435a6226c4b5b00c6ed8
Rodriasd <veronrodrigo98@gmail.com>
2024-04-26 10:07:57 -0300

    was added bar graphic

--- app.py
+++ app.py
@@ -13,4 +13,12 @@ plt.grid(True)
 data = sample(range(1, 1000), 100)
 plt.hist(data,bins = 10)
 plt.title("Histograma")
+
+#Grafico de torta
+labels = 'Caballos', 'Cerdos', 'Perros', 'Vacas'
+sizes = [15, 30, 45, 10]
+explode = (0, 0.1, 0, 0)
+plt.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',
+shadow=True, startangle=90)
+plt.axis('equal')
 plt.show()
